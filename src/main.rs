20ddd66b13e79b2d37b19bc9dc55968c18502ef1
hal <hal@sample.com>
2023-11-09 22:23:41 +0900

    wip

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+
+use std::path::PathBuf;
+use std::fs;
+use std::io::{self, BufRead};
+
+#[macro_use]
+extern crate lazy_static;
+use regex::Regex;
+use clap::Parser;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"[^\s]+:jar:[^\s]+:").unwrap();
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// ${HOME}/.m2/repository
+    /// marven dependency:treeの実行結果を張り付けたテキストファイル
+    #[clap(value_parser)]
+    repo_file: std::path::PathBuf,
+
+    /// jarの配置先ディレクトリ
+    #[clap(value_parser)]
+    target_dir: std::path::PathBuf,
+}
+
+/// jar fileへのパスを返す
+fn parse(m2_repo_path: &PathBuf,line: String) -> Result<PathBuf, String> {
+    let mut path = PathBuf::from(m2_repo_path);
+    if !line.contains("jar") {
+        let msg = format!("{line} does not contain jar path.");
+        return Err(msg)
+    }
+    if let Some(s) = RE
+            .find(
+                line
+                .as_str())
+                .map(|m| m.as_str().to_string()
+            )
+    {
+        //      group          | artifact     |type| version
+        // path com.google.api:api-common:jar:1.10.4
+        // m2_repo_path/group/artifact/version/artifact-version.jar
+        let p: Vec<&str> = s.split(":").collect();
+        if p.len() != 5 {
+            return Err(format!("{line} is invalid format line."));
+        }
+        let group = p[0].replace(".", "/");
+        let artifact = p[1];
+        let version = p[3];
+        path.push(group);
+        path.push(artifact);
+        path.push(version);
+        path.push(format!("{artifact}-{version}.jar"));
+        return Ok(path)
+    }
+    Err(format!("{line} does not match regex pattern"))
+}
+
+fn _copy_jar(jars: Vec<PathBuf>, target_dir: PathBuf) -> io::Result<()>{
+    for jar_path in jars {
+        if jar_path.exists() && jar_path.is_file() {
+            let mut dest = target_dir.clone();
+            dest.push(jar_path.file_name().unwrap());
+            if dest.exists() {
+                println!("skip target=[{dest:?}. because jar=[{:?}]] exist.", jar_path.file_name().unwrap());
+                continue;
+            }
+            fs::copy(jar_path, dest)?;
+        } else {
+            println!("skip... target=[{jar_path:?}]");
+        }
+    }
+    Ok(())
+}
+
+fn copy_jar_to_target_dir(m2_repo_path: PathBuf,repo_file:PathBuf, target_dir:PathBuf) -> io::Result<()>{
+    let mut jars = Vec::<PathBuf>::new();
+    { // open file
+        let file = fs::File::open(repo_file)?;
+        let reader = io::BufReader::new(file);
+        for line in reader.lines() {
+            let line = line?;
+            match parse(&m2_repo_path, line) {
+                Ok(p) => jars.push(p),
+                Err(e) => println!("error = {e}"),
+            }
+        }
+    } // close file
+    _copy_jar(jars, target_dir)?;
+    Ok(())
+}
+
+fn main() {
+    let args = Args::parse();
+    
+    // $HOME/.m2/repositoryが存在するか確認
+    let home_var = std::env::var("HOME")
+        .expect("HOME enviroment varivale not set ");
+    let mut m2_repo_path = std::path::PathBuf::from(home_var);
+    m2_repo_path.push(".m2");
+    m2_repo_path.push("repository");
+    if !m2_repo_path.exists() || !m2_repo_path.is_dir() {
+        eprintln!(".m2 repository direcory doesn't exsit. path={m2_repo_path:?}");
+        std::process::exit(1);
+    }
+
+    if !args.repo_file.exists() || !args.repo_file.is_file() {
+        eprintln!("repo file doesn't exsit. path={:?}", args.repo_file);
+        std::process::exit(1);
+    }
+
+    if !args.target_dir.exists() || !args.target_dir.is_dir() {
+        eprintln!("target dir doesn't exsit. path={:?}", args.repo_file);
+        std::process::exit(1);
+    }
+
+    if let Err(e) = copy_jar_to_target_dir(
+            m2_repo_path,
+            args.repo_file,
+            args.target_dir
+    ) {
+        eprintln!("Error occured. error={e}");
+        std::process::exit(1);
+    }
+
+}
